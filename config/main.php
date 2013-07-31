f8b8b16f0fb1d4f0ca8252db003155d3784792b7
root <anotherforwork@gmail.com>
2013-07-31 17:52:22 +0300

    commit-test

--- config/main.php
+++ config/main.php
@@ -1,7 +1,7 @@
 <?php 
 $_API_SETTINGS = array(
 	'upload_path' => 'upload',
-	'allowed_types' => array('png','jpg','gif','tga','my'),
+	'allowed_types' => array('png','jpg','gif','tga','my','newmy'),
 	'default_image_prefix' => 'pic',
 	);
 ?>
